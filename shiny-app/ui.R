f387bc5c7016c97fe781dba09c062c132d063e42
Yanal Kashou <yanal.kashou@gmail.com>
2016-08-21 18:27:41 +0300

    Implemented Shiny Dashboard. Implemented googleVis.

--- shiny-app/ui.R
+++ shiny-app/ui.R
@@ -1,19 +1,47 @@
 library(shiny)
+library(shinydashboard)
 
-shinyUI(pageWithSidebar(
-    headerPanel("Earthquake Magnitude visualization"),
-    sidebarPanel(
-        sliderInput('i', 'Earthquake Magnitude', 4, min = 0, max = 8, step = 0.1)
-    ),
-
-    mainPanel(
-       plotOutput('newMap',
-       dblclick = "newMap_dblclick",
-       brush = brushOpts(
-         id = "newMap_brush",
-         resetOnNew = TRUE
-       )),
-       dataTableOutput( "results" )
+shinyUI(dashboardPage(
+  dashboardHeader(title = "Earthquakes"),
+  dashboardSidebar(
+    sidebarMenu(
+    menuItem("Overview", tabName = "overview", icon = icon("dashboard"), 
+             menuSubItem("Tables", tabName = "tables", icon = icon("table"))),
+    menuItem("Widgets", tabName = "widgets", icon = icon("th")),
+    menuItem("googleVis", tabName = "googleVis", icon = icon ("map")),
+    menuItem("Source Code", tabName = "sourcecode", icon = icon ("code"))
+  )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "overview",
+              h2("Overview"),
+              p("Hi")
+      ),
+      tabItem(tabName = "tables",
+              h2("Table"),
+              tableOutput("table1")
+      ),
+      tabItem(tabName = "widgets",
+              h2("Widgets")
+      ),
+      tabItem(tabName = "googleVis",
+              h2("Earthquake Visualization using googleVis"),
+              h3("Using gvisMap"),
+              htmlOutput("gvis1"),
+              h3("Using gvisGeoChart"),
+              htmlOutput("gvis2")
+      ),
+      tabItem(tabName = "sourcecode",
+              h2("Source Code"),
+              h3("ui.R"),
+              tags$a(href="https://github.com/ykashou92/DevelopingDataProducts/blob/master/shiny-app/ui.R", "ui.R", icon = icon ("github")),
+              h3("server.R"),
+              tags$a(href="https://github.com/ykashou92/DevelopingDataProducts/blob/master/shiny-app/server.R", "server.R")
+      )
     )
-    )
+  )
 )
+)
+
+
